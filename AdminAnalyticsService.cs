e9293983e7bd42045a9501d3b2d757fbb180fe6c
Julianna Diaz <jdjuliannadiaz@gmail.com>
2018-06-10 20:52:50 -0700

    Add files via upload

---
+++ AdminAnalyticsService.cs
@@ -0,0 +1,98 @@
+﻿using Web.Data.Providers;
+using Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Services
+{
+    public class AdminAnalyticsService : IAdminAnalyticsService
+    {
+        readonly IDataProvider dataProvider;
+
+        public AdminAnalyticsService(IDataProvider dataProvider)
+        {
+            this.dataProvider = dataProvider;
+        }
+
+        public string GetAll()
+        {
+            var jsonResult = new StringBuilder();
+            dataProvider.ExecuteCmd(
+                "AdminAnalytics_GetAll",
+                inputParamMapper: null,
+                singleRecordMapper: (reader, resultSetId) =>
+                {
+                    jsonResult.Append(reader.GetString(0));
+                });
+            return jsonResult.ToString();
+        }
+
+        public string GetByDay()
+        {
+            var jsonResult = new StringBuilder();
+            dataProvider.ExecuteCmd(
+                "AdminAnalytics_GetByDay",
+                inputParamMapper: null,
+                singleRecordMapper: (reader, resultSetId) =>
+                {
+                    jsonResult.Append(reader.GetString(0));
+                });
+            return jsonResult.ToString();
+        }
+
+        public string GetByWeek()
+        {
+            var jsonResult = new StringBuilder();
+            dataProvider.ExecuteCmd(
+                "AdminAnalytics_GetByWeek",
+                inputParamMapper: null,
+                singleRecordMapper: (reader, resultSetId) =>
+                {
+                    jsonResult.Append(reader.GetString(0));
+                });
+            return jsonResult.ToString();
+        }
+
+        public string GetByMonth()
+        {
+            var jsonResult = new StringBuilder();
+            dataProvider.ExecuteCmd(
+                "AdminAnalytics_GetByMonth",
+                inputParamMapper: null,
+                singleRecordMapper: (reader, resultSetId) =>
+                {
+                    jsonResult.Append(reader.GetString(0));
+                });
+            return jsonResult.ToString();
+        }
+
+        public string GetBy6Months()
+        {
+            var jsonResult = new StringBuilder();
+            dataProvider.ExecuteCmd(
+                "AdminAnalytics_GetBy6Months",
+                inputParamMapper: null,
+                singleRecordMapper: (reader, resultSetId) =>
+                {
+                    jsonResult.Append(reader.GetString(0));
+                });
+            return jsonResult.ToString();
+        }
+
+        public string GetByYear()
+        {
+            var jsonResult = new StringBuilder();
+            dataProvider.ExecuteCmd(
+                "AdminAnalytics_GetByYear",
+                inputParamMapper: null,
+                singleRecordMapper: (reader, resultSetId) =>
+                {
+                    jsonResult.Append(reader.GetString(0));
+                });
+            return jsonResult.ToString();
+        }
+    }
+}
